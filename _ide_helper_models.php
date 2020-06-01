34b864921707c3d2d893fa270710caa4ce743852
Bondarenkosv <bondarenkosv13@gmail.com>
2020-06-01 23:02:35 +0300

    read the routes (Products, Categories, Orders and Users)

---
+++ _ide_helper_models.php
@@ -0,0 +1,215 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property string $sku
+ * @property string $name
+ * @property string|null $description
+ * @property string $short_description
+ * @property float $price
+ * @property int $discount
+ * @property int $quantity
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Category[] $categories
+ * @property-read int|null $categories_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Image[] $image
+ * @property-read int|null $image_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Order[] $orders
+ * @property-read int|null $orders_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Image[] $thumbnail
+ * @property-read int|null $thumbnail_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereDiscount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereQuantity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereShortDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereSku($value)
+ */
+	class Product extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\OrdersStatus
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $type
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Order[] $orders
+ * @property-read int|null $orders_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrdersStatus newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrdersStatus newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrdersStatus query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrdersStatus whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrdersStatus whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrdersStatus whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrdersStatus whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\OrdersStatus whereUpdatedAt($value)
+ */
+	class OrdersStatus extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Image
+ *
+ * @property int $id
+ * @property string $path
+ * @property int $imageable_id
+ * @property string $imageable_type
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $imageable
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Product[] $product
+ * @property-read int|null $product_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Image newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Image newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Image query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Image whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Image whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Image whereImageableId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Image whereImageableType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Image wherePath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Image whereUpdatedAt($value)
+ */
+	class Image extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Order
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $user_name
+ * @property string $user_surname
+ * @property string $user_email
+ * @property string $user_phone
+ * @property string $country
+ * @property string $city
+ * @property string $address
+ * @property float $total
+ * @property int $status_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Product[] $products
+ * @property-read int|null $products_count
+ * @property-read \App\Models\OrdersStatus $status
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereCountry($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereStatusId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereTotal($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereUserEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereUserName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereUserPhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Order whereUserSurname($value)
+ */
+	class Order extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Role
+ *
+ * @property int $id
+ * @property string $name
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\User[] $Users
+ * @property-read int|null $users_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role whereName($value)
+ */
+	class Role extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Category
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property-read \App\Models\Image|null $image
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Product[] $products
+ * @property-read int|null $products_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category whereName($value)
+ */
+	class Category extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property int $role_id
+ * @property string $name
+ * @property string $surname
+ * @property string $birth_date
+ * @property string $email
+ * @property string $phone
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Image|null $image
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Order[] $orders
+ * @property-read int|null $orders_count
+ * @property-read \App\Models\Role $role
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereBirthDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereRoleId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereSurname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,37 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::name('admin.')->group(function () {
+    Route::get('orders', 'OrdersController@index')->name('orders'); //show all orders
+    Route::get('{id}/orders', 'OrdersController@show')->name('orders.show'); // show the order
+    Route::get('orders/create', 'OrdersController@create')->name('orders.create'); // view create a order
+    Route::post('orders/store', 'OrdersController@store')->name('orders.store'); // store to create the order
+    Route::get('orders/{id}/edit', 'OrdersController@edit')->name('orders.edit'); // view edit the order
+    Route::post('orders/{id}/update', 'OrdersController@update')->name('orders.update'); // update the order
+    Route::delete('orders/{id}/delete', 'OrdersController@delelet')->name('orders.delete'); // delete the order
+
+    Route::get('categories', 'CategoriesController@index')->name('categories'); //show all categories
+    Route::get('{id}/categories', 'CategoriesController@show')->name('categories.show'); // show the category
+    Route::get('categories/create', 'CategoriesController@create')->name('categories.create'); // view create a category
+    Route::post('categories/store', 'CategoriesController@store')->name('categories.store'); // store to create the category
+    Route::get('categories/{id}/edit', 'CategoriesController@edit')->name('categories.edit'); // view edit the category
+    Route::post('categories/{id}/update', 'CategoriesController@update')->name('categories.update'); // update the category
+    Route::delete('categories/{id}/delete', 'CategoriesController@delelet')->name('categories.delete'); // delete the category
+
+    Route::get('products', 'ProductController@index')->name('products'); //show all products
+    Route::get('{id}/products', 'ProductController@show')->name('products.show'); // show the product
+    Route::get('products/create', 'ProductController@create')->name('products.create'); // view create a product
+    Route::post('products/store', 'ProductController@store')->name('products.store'); // store to create the product
+    Route::get('products/{id}/edit', 'ProductController@edit')->name('products.edit'); // view edit the product
+    Route::post('products/{id}/update', 'ProductController@update')->name('products.update'); // update the product
+    Route::delete('products/{id}/delete', 'ProductController@delelet')->name('products.delete'); // delete the product
+
+    Route::get('users', 'UsersController@index')->name('users'); //show all users
+    Route::get('{id}/users', 'UsersController@show')->name('users.show'); // show the user
+    Route::delete('users/{id}/delete', 'UsersController@delelet')->name('users.delete'); // delete the user
+});
