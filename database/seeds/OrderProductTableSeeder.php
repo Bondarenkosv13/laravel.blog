ae12a6c3b55f26cadb7a50700e48b55b17536101
Bondarenkosv <bondarenkosv13@gmail.com>
2020-06-06 23:02:31 +0300

    Finish write OrderProductTableSeeder. I used "attach" instead "updateExistingPivot".

--- database/seeds/OrderProductTableSeeder.php
+++ database/seeds/OrderProductTableSeeder.php
@@ -11,37 +11,18 @@ class OrderProductTableSeeder extends Seeder
      */
     public function run()
     {
+        factory(\App\Models\Order::class, 10)->create()->each(function (\App\Models\Order $order) {
+            $countOfProduct = rand(1, 5);
+            for($i = 0; $i < $countOfProduct; $i++) {
+                $quantity = rand(1, 5);
+                $products = \App\Models\Product::where('id', '=', rand(1, 5))->first();;
 
-
-        $count = 10;
-        for($i=0; $i < $count; $i++) {
-            $quantity = rand(1, 20);
-            $product = \App\Models\Product::where('id', '=', rand(1, 5))->first();
-            $order = \App\Models\Order::where('id', '=', rand(1, 5))->first();
-
-            DB::table('order_product')->insert([
-                'order_id' => $order->id,
-                'product_id' => $product->id,
-                'quantity' => $quantity,
-                'price' => $product->price
-            ]);
-        }
-/**
- *  Код ниже с урока не рабоатет, не понимаю почему, не записываются данные в таблицу.
- * Вывел переменную $Prod, посмотреть что дает, рещультат = 0. Хз что делать. Сделал так как понял сам вверху.
- */
-//        factory(\App\Models\Order::class, 10)->create()->each(function (\App\Models\Order $order) {
-//            $countOfProduct = rand(1, 5);
-//            for($i = 0; $i < $countOfProduct; $i++) {
-//                $quantity = rand(1, 5);
-//                $products = \App\Models\Product::all()->random();
-//
-//               $Prod =  $products->orders()->updateExistingPivot($order, [
-//                    'quantity' => $quantity,
-//                    'price'    => $products->price
-//                ]);
-//            }
-//        });
+               $products->orders()->attach($order, [
+                    'quantity' => $quantity,
+                    'price'    => $products->price
+                ]);
+               }
+        });
 
     }
 }
